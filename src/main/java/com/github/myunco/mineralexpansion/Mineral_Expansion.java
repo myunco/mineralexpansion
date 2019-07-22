1b2165c7de9aebc95f9a8fe8ff178e14686a644a
myunco <myunco@qq.com>
2019-07-22 21:14:51 +0800

    fix bugs

--- src/main/java/com/github/myunco/mineralexpansion/Mineral_Expansion.java
+++ src/main/java/com/github/myunco/mineralexpansion/Mineral_Expansion.java
@@ -16,7 +16,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class Mineral_Expansion {
     public static final String MODID = "mineral_expansion";
     public static final String NAME = "Mineral_Expansion";
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.0.5";
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
